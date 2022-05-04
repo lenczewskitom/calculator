c7b0351a5f409da0478dfffde14fbcd1a7907a0e
lenczewskitom <lenczewskitom@gmail.com>
2022-05-04 12:45:16 +0200

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+class Calculator {
+
+    public int A = 10;
+    public int B = 5;
+
+    public void addNumbers() {
+        System.out.println("A + B = " + (A + B));
+    }
+
+    public void substractNumbers() {
+        System.out.println("A - B = " + (A-B));
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Calculator calculator1 = new Calculator();
+
+        calculator1.addNumbers();
+        calculator1.substractNumbers();
+
     }
 }
